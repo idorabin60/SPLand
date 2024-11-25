725deee73135fdc9b56f79898876c9518181f153
idorabin60 <idorabin60@gmail.com>
2024-11-25 08:13:32 +0000

    move DS Store

--- src/Auxiliary.cpp
+++ src/Auxiliary.cpp
@@ -18,5 +18,5 @@ std::vector<std::string> Auxiliary::parseArguments(const std::string &line)
         arguments.push_back(argument);
     }
 
-    return arguments; // test
+    return arguments; // testss
 }
